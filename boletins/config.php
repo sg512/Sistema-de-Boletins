8f93469bd66aa32d8aea8bf073a5d98c11613544
sg512 <sg512.sef@outlook.com>
2016-08-01 14:36:58 -0300

    Dados de Acesso
    Remoção de dados de acesso.

--- boletins/config.php
+++ boletins/config.php
@@ -1,24 +1,10 @@
 <?php
-define('URL', 'http://intranet.sef.eb.mil.br/boletins/');
+define('URL', 'http://localhost/mvc/');
 define('LIBS', 'libs/');
 define('SERVIDOR_DE_ARQUIVO', '//var/www//boletins//public//uploads//');
 define('DB_TYPE', 'mysql');
-define('DB_HOST', '127.1.1.1');
-define('DB_NAME', 'boletins');
-define('DB_USER', 'root');
-define('DB_PASS', 'Sentinelasef2k84761524');
-define('HASH_GENERAL_KEY', 'MixitUp200');
-define('HASH_PASSWORD_KEY', 'catsFLYhigh2000miles');
-
-
-/*
-// Always provide a TRAILING SLASH (/) AFTER A PATH
-define('URL', 'http://localhost/mvc/');
-define('LIBS', 'libs/');
-
-define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'mvc');
+define('DB_NAME', 'boletins');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
@@ -28,5 +14,3 @@ define('HASH_GENERAL_KEY', 'MixitUp200');
 
 // This is for database passwords only
 define('HASH_PASSWORD_KEY', 'catsFLYhigh2000miles');
- */
-
